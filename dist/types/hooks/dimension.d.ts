fd5d6855092a56613f11998b66768f282e20539e
HenryLF <henrylf@protonmail.com>
2025-08-19 20:53:29 +0200

    optimize

--- dist/types/hooks/dimension.d.ts
+++ dist/types/hooks/dimension.d.ts
@@ -1,7 +1,7 @@
 export declare function useDimension(): {
     height: number;
     width: number;
-};
+} | null;
 export declare function useResponsive(breakpoints?: Record<string, number>): string[] | null;
-export declare function useBreakPoints<T>(breakpoints?: Record<number, T>): T | null;
+export declare function useBreakPoints<T>(breakpoints: Record<number, T>): T | null;
 //# sourceMappingURL=dimension.d.ts.map
